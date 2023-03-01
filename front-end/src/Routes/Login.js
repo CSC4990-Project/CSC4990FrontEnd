3467f804a5b23a34b0bd4a715ee2b536a38a26dc
JacobDarmstadt <55498881+JacobDarmstadt@users.noreply.github.com>
2023-03-01 10:21:10 -0600

    login created

--- front-end/src/Routes/Login.js
+++ front-end/src/Routes/Login.js
@@ -1,7 +1,26 @@
+import {useState} from "react";
+import {useHistory} from "react-router-dom";
+import "../App.css"
+//TODO Style login create functionality with db
 const Login = ()=>{
+    const [email, setEmail] = useState("")
+    const [pass,setPass] = useState("")
+    const history = useHistory()
+
+    function handleSubmit() {
+        history.push("/")
+
+    }
+
     return(
-        <div>
-            <h1>This is the login</h1>
+        <div className="main-content">
+            <form className={"form-content"} onSubmit={handleSubmit}>
+                <label for="email">Email:</label>
+                <input value={email}onChange={(e)=> setEmail(e.target.value)}/>
+                <label for="password">Password</label>
+                <input value={pass} onChange={(e)=>setPass(e.target.value)}/>
+                <button className={'button-content'} type="submit">Login</button>
+            </form>
         </div>
     )
 }
