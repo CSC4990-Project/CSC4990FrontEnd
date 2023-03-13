409c206365ef59002760f1637df9945c737bb3a8
JacobDarmstadt <55498881+JacobDarmstadt@users.noreply.github.com>
2023-03-13 10:11:45 -0500

    Login creation
    created login page

--- front-end/src/Routes/Login.js
+++ front-end/src/Routes/Login.js
@@ -1,7 +1,35 @@
+import {useState} from "react";
+import {useHistory} from "react-router-dom";
+import background from "../Images/auroraUniversity.jpg"
+import logo from "../Images/Aurora_University_logo.jpg"
+import "../App.css"
+//create functionality with db
 const Login = ()=>{
+    const [email, setEmail] = useState("")
+    const [pass,setPass] = useState("")
+    const history = useHistory()
+
+    function handleSubmit() {
+        history.push("/")
+
+    }
+
     return(
-        <div>
-            <h1>This is the login</h1>
+
+        <div style={{backgroundImage: `url(${background})`,
+            width:'100%',
+        height:'600px'}}>
+
+        <div className="Login">
+            <img src={logo} alt='aurora university logo'/>
+            <form onSubmit={handleSubmit}>
+                <label for="email">Email</label>
+                <input value={email}onChange={(e)=> setEmail(e.target.value)}/>
+                <label for="password">Password</label>
+                <input value={pass} onChange={(e)=>setPass(e.target.value)}/>
+                <button className={'button-content'} type="submit">Login</button>
+            </form>
+        </div>
         </div>
     )
 }
