592335b39e6db9f82a386da5d0e669d299b59751
JacobDarmstadt <55498881+JacobDarmstadt@users.noreply.github.com>
2023-03-15 09:56:53 -0500

    Login Creation

--- front-end/src/Routes/Login.js
+++ front-end/src/Routes/Login.js
@@ -1,3 +1,4 @@
+
 import {useState} from "react";
 import {useHistory} from "react-router-dom";
 import background from "../Images/auroraUniversity.jpg"
@@ -18,18 +19,19 @@ const Login = ()=>{
 
         <div style={{backgroundImage: `url(${background})`,
             width:'100%',
-        height:'600px'}}>
+            height:'600px'}}>
 
-        <div className="Login">
-            <img src={logo} alt='aurora university logo'/>
-            <form onSubmit={handleSubmit}>
-                <label for="email">Email</label>
-                <input value={email}onChange={(e)=> setEmail(e.target.value)}/>
-                <label for="password">Password</label>
-                <input value={pass} onChange={(e)=>setPass(e.target.value)}/>
-                <button className={'button-content'} type="submit">Login</button>
-            </form>
-        </div>
+            <div className="Login">
+                <img src={logo} alt='aurora university logo'/>
+                <form onSubmit={handleSubmit}>
+                    <label for="email">Email</label>
+
+                    <input value={email}onChange={(e)=> setEmail(e.target.value)} required/>
+                    <label for="password">Password</label>
+                    <input value={pass} onChange={(e)=>setPass(e.target.value)} required/>
+                    <button className={'button-content'} type="submit">Login</button>
+                </form>
+            </div>
         </div>
     )
 }
