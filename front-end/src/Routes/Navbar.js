93e60d963a987681248672d203e7f1d9f91350d5
JacobDarmstadt <55498881+JacobDarmstadt@users.noreply.github.com>
2023-03-16 22:06:42 -0500

    created navbar

--- front-end/src/Routes/Navbar.js
+++ front-end/src/Routes/Navbar.js
@@ -1,8 +1,26 @@
+import logo from "../Images/Aurora_University_logo.jpg";
+
 const Navbar = ()=>{
     return(
-        <div>
-            <h1>This is the navbar</h1>
-        </div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <img src={logo} style={{height:'75px', margin:'0 0 0 15px'}} alt='aurora university logo'/>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <a className="nav-link" href="/">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="create">Create Form</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="ticket">Your Tickets</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="login">Login</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 export default Navbar;
