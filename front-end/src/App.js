b443c1e56ad6d016fb3d0dc3139f9881fb4cfe0f
JacobDarmstadt <55498881+JacobDarmstadt@users.noreply.github.com>
2023-03-19 06:01:50 -0500

    login functionality register and authentication

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,25 +5,42 @@ import Login from "./Routes/Login";
 import Navbar from "./Routes/Navbar";
 import Ticket from "./Routes/Ticket";
 import Form from "./Routes/Form";
+import Register from "./Routes/Register"
+import {useEffect, useState} from "react";
 
 function App() {
+    const [name,setName] = useState('')
+    const [fresh,setFresh] = useState(true)
+    useEffect(()=>{
+        (
+            async ()=>{
+                const response = await fetch("http://localhost:8000/api/user", {
+                    method:"GET",
+                    headers:{'Content-Type':'application/json'},
+                    credentials:'include',
+                })
+                const content = await response.json()
+                setName(content.email)
+            }
+        )();
+    });
     return (
       <Router>
         <div className="App">
-            <Navbar/>
+            <Navbar name={name} setName={setName}/>
             <div className={"content"}>
             <Switch>
-              <Route exact path="/">
-                  <About/>
-              </Route>
-              <Route exact path="/login">
-                <Login/>
-              </Route>
+              <Route exact path="/" component={()=><About name={name} fresh={fresh} setFresh={setFresh}/>}/>
+
+              <Route exact path="/login" component={()=><Login name={name} setName={setName} setFresh={setFresh}/>}/>
                 <Route exact path="/ticket">
                     <Ticket/>
                 </Route>
                 <Route exact path="/create">
                     <Form/>
+                </Route>
+                <Route exact path="/register">
+                    <Register/>
                 </Route>
             </Switch>
           </div>
