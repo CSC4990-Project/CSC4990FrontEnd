d0368a930d741ff28045cd156c020dcb69abd626
Jperez46 <55904134+Jperez46@users.noreply.github.com>
2023-03-31 12:29:26 -0500

    Update About.js

--- front-end/src/Routes/About.js
+++ front-end/src/Routes/About.js
@@ -10,18 +10,18 @@ const About = (props)=>{
     return(
         <div>
 
-            <h1>{props.name?'Hi ' + props.name:'you are not logged in'}</h1>
-            <h1>About Page</h1>
-            <p> Welcome to the Aurora university report a problem app. Here you can submit reports abouts
+            <h1 className = "logged">{props.name?'Hi ' + props.name:'you are not logged in'}</h1>
+            <h1 className = "title">About Page</h1>
+            <p className = "desc"> Welcome to the Aurora university report a problem app. Here you can submit reports abouts
                 issues on campus by filling out a form. You can also keep track of the progress on any of your previously reported
                 problems.
-                IF THIS IS AN EMERGENCY CALL 911 DO NOT USE THE APP!!!
+                <b>IF THIS IS AN EMERGENCY CALL 911 DO NOT USE THE APP!!!</b>
                 Below you can find the steps on how you report a problem.
-                <ol>
-                    <li> Click the Report a problem button</li>
-                    <li> Fill out the form and if you want you can submit a picture to help explain the problem</li>
-                    <li>Submit the form </li>
-                    <li> Now your done</li>
+                <ol className = "list">
+                    <li className="list1"> Click the Report a problem button</li>
+                    <li className="list2"> Fill out the form and if you want you can submit a picture to help explain the problem</li>
+                    <li className="list3">Submit the form </li>
+                    <li className="list4"> Now your done</li>
                 </ol>
                 Your problem has now be submitted you can view the progress on the issue in the view progress menu.
             </p>
